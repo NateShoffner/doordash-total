0d2d204182d938a4edbe62ea9892c722a3df66b6
Nate Shoffner <nate.shoffner@gmail.com>
2024-10-11 23:14:35 -0400

    Monthly breakdown

--- calculate.py
+++ calculate.py
@@ -1,3 +1,4 @@
+import datetime
 from client import Client
 
 def load_cookies(filename: str) -> dict:
@@ -21,25 +22,37 @@ orders = client.get_orders()
 total = 0
 annual = 0
 
-yearly_spend = {}
+yearly_spend = {} # {year: {month: {day: {total}}}}
 
+# track spend for each day of the year
 for order in orders:
-
     if order.cancelledAt:
         continue
 
     total += order.grandTotal.unitAmount
 
-    order_year = order.createdAt.split("-")[0]
+    order_year, order_month, order_day = order.createdAt.split("-")
 
     if order_year not in yearly_spend:
-        yearly_spend[order_year] = 0
-    else:
-        yearly_spend[order_year] += order.grandTotal.unitAmount
+        yearly_spend[order_year] = {}
+    if order_month not in yearly_spend[order_year]:
+        yearly_spend[order_year][order_month] = {}
+    if order_day not in yearly_spend[order_year][order_month]:
+        yearly_spend[order_year][order_month][order_day] = 0
+
+    yearly_spend[order_year][order_month][order_day] += order.grandTotal.unitAmount
 
 print("\nSpending by year:")
 
-for year, spend in yearly_spend.items():
-    print(f"{year}: ${spend / 100}")
+for year, months in yearly_spend.items():
+    print(f"\n{year}:")
+    year_total = 0
+    for month, days in months.items():
+        month_name = datetime.datetime.strptime(month, "%m").strftime("%B")
+        monthly_sum = sum(days.values())
+        year_total += monthly_sum
+        print(f"  {month_name}: ${monthly_sum / 100}")
+
+    print(f"\n  ----------\n  Total: ${year_total / 100}")
 
 print(f"\nTotal: ${total / 100}")
